51c2b4f25b632d739c4a258457c6986f67ed8708
true1ck <thunderblunder018@gmail.com>
2026-01-02 01:24:09 +0530

    latest a s

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://pandapaths.com';
+    const baseUrl = 'https://pandapath.in';
 
     // Static pages
     const staticPages = [
